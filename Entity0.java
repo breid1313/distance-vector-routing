ee8d54a6f85bcf00a2081fe2c3dfcdf268d1aab4
Brian Reid <breid1313@gmail.com>
2021-11-26 22:55:52 -0500

    fix some bugs, apply to entity 1-3 as well

--- Entity0.java
+++ Entity0.java
@@ -1,11 +1,13 @@
 public class Entity0 extends Entity
 {   
     // constant number of nodes
-    public static final int numEntities = NetworkSimulator.NUMENTITIES;
+    int numEntities = NetworkSimulator.NUMENTITIES;
+    // constant array of neighbor node indexes (to aid in iteration)
+    int[] neighbors = new int[] {0, 1, 3};
     // Perform any necessary initialization in the constructor
     public Entity0()
     {
-        // initialize all values in the DT to 0 when the node is created
+        // initialize all values in the DT to inf when the node is created
         for (int i = 0 ; i < numEntities; i++) {
             for (int j = 0; j < numEntities; j++) {
                 distanceTable[i][j] = 999;
@@ -24,16 +26,16 @@ public class Entity0 extends Entity
         int[] minDistances = new int[numEntities];
         for (int i = 0; i < numEntities; i++) {
             int minDist = 999;
-            for (int j = 1; j < numEntities; j++) {
-                if (distanceTable[i][j] < minDist) {
-                    minDist = distanceTable[i][j];
+            for (int j = 0; j < neighbors.length; j++) {
+                if (distanceTable[i][neighbors[j]] < minDist) {
+                    minDist = distanceTable[i][neighbors[j]];
                 }
             }
             minDistances[i] = minDist;
         }
 
-        // broadcast mindistances to the other nodes
-        for(int i = 1; i < numEntities; i++) {
+        // broadcast min distances to the other nodes
+        for (int i = 1; i < numEntities; i++) {
             Packet packet = new Packet(0, i, minDistances);
             NetworkSimulator.toLayer2(packet);
         }
@@ -59,9 +61,9 @@ public class Entity0 extends Entity
         int[] minDistances = new int[numEntities];
         for (int i = 0; i < numEntities; i++) {
             int minDist = 999;
-            for (int j = 1; j < numEntities; j++) {
-                if (distanceTable[i][j] < minDist) {
-                    minDist = distanceTable[i][j];
+            for (int j = 0; j < neighbors.length; j++) {
+                if (distanceTable[i][neighbors[j]] < minDist) {
+                    minDist = distanceTable[i][neighbors[j]];
                 }
             }
             minDistances[i] = minDist;
@@ -81,8 +83,8 @@ public class Entity0 extends Entity
         }
 
         // broadcast the change if one occurred during update
-        System.out.println("Detected a min distance change at Entity0... broadcasting to other nodes.\n")
         if( changed ){
+            System.out.println("Detected a min distance change at Entity0... broadcasting to other nodes.\n");
             for (int i = 1; i < numEntities; i++) {
                 Packet packet = new Packet(0, i, minDistances);
                 NetworkSimulator.toLayer2(packet);
@@ -92,7 +94,7 @@ public class Entity0 extends Entity
         // debug/info logging
         System.out.println("Entity0 Updated at:" + NetworkSimulator.time + ". Distance Table is:\n");
         printDT();
-
+    }
     
     public void linkCostChangeHandler(int whichLink, int newCost)
     {
@@ -115,14 +117,14 @@ public class Entity0 extends Entity
         distanceTable[whichLink][whichLink] = newCost;
 
         // calc min distances as before
-                // calculate our minimum costs, as in the init method
+        // calculate our minimum costs, as in the init method
         // TODO would be nice to extract this and reuse...
         int[] minDistances = new int[numEntities];
         for (int i = 0; i < numEntities; i++) {
             int minDist = 999;
-            for (int j = 1; j < numEntities; j++) {
-                if (distanceTable[i][j] < minDist) {
-                    minDist = distanceTable[i][j];
+            for (int j = 0; j < neighbors.length; j++) {
+                if (distanceTable[i][neighbors[j]] < minDist) {
+                    minDist = distanceTable[i][neighbors[j]];
                 }
             }
             minDistances[i] = minDist;
--- Entity1.java
+++ Entity1.java
@@ -1,8 +1,49 @@
 public class Entity1 extends Entity
 {    
+    // constant number of nodes
+    int numEntities = NetworkSimulator.NUMENTITIES;
+    // constant array of neighbor node indexes (to aid in iteration)
+    int[] neighbors = new int[] {0, 2};
     // Perform any necessary initialization in the constructor
     public Entity1()
     {
+        // initialize all values in the DT to inf when the node is created
+        for (int i = 0 ; i < numEntities; i++) {
+            for (int j = 0; j < numEntities; j++) {
+                distanceTable[i][j] = 999;
+            }
+        }
+
+        // set the values in the distance table that we know from the diagram
+        // 1 is not connected to three so we initialize it to inf
+        // TODO if time: would be nice to make this a little more dynamic. Hard code for now.
+        // Could set neighbors nodes and edges up top and calculate this maybe.
+        distanceTable[0][0] = 1;
+        distanceTable[1][1] = 0;
+        distanceTable[2][2] = 1;
+        distanceTable[3][3] = 999;
+
+        // store the known minimum distances from node0 to all over nodes
+        int[] minDistances = new int[numEntities];
+        for (int i = 0; i < numEntities; i++) {
+            int minDist = 999;
+            for (int j = 0; j < neighbors.length; j++) {
+                if (distanceTable[i][neighbors[j]] < minDist) {
+                    minDist = distanceTable[i][neighbors[j]];
+                }
+            }
+            minDistances[i] = minDist;
+        }
+
+        // broadcast mindistances to the other nodes
+        for(int i = 1; i < numEntities; i++) {
+            Packet packet = new Packet(1, i, minDistances);
+            NetworkSimulator.toLayer2(packet);
+        }
+
+        // debug/info logging
+        System.out.println("Entity1 Initialized at:" + NetworkSimulator.time + ". Distance Table is:\n");
+        printDT();
     }
     
     // Handle updates when a packet is received.  Students will need to call
@@ -12,10 +53,94 @@ public class Entity1 extends Entity
     // details.
     public void update(Packet p)
     {
+        // mark whether we need to broadcast an update to the rest of the nodes
+        // if this becomes true we should send.
+        boolean changed = false;
+
+        // calculate our minimum costs, as in the init method
+        // TODO would be nice to extract this and reuse...
+        int[] minDistances = new int[numEntities];
+        for (int i = 0; i < numEntities; i++) {
+            int minDist = 999;
+            for (int j = 0; j < neighbors.length; j++) {
+                if (distanceTable[i][neighbors[j]] < minDist) {
+                    minDist = distanceTable[i][neighbors[j]];
+                }
+            }
+            minDistances[i] = minDist;
+        }
+        
+        for(int i = 0; i < numEntities; i++){
+            // update the distance table if it's cheaper to get to a node from p's source
+            if ( p.getMincost(i) + minDistances[p.getSource()] < distanceTable[i][p.getSource()] ) {
+                distanceTable[i][p.getSource()] = p.getMincost(i) + minDistances[p.getSource()];
+                // update the min distance if we've found a cheaper route and mark
+                // the we need to broadcast a change
+                if (distanceTable[i][p.getSource()] < minDistances[i]) {
+                    minDistances[i] = distanceTable[i][p.getSource()];
+                    changed = true;
+                  }
+            }
+        }
+
+        // broadcast the change if one occurred during update
+        if( changed ){
+            System.out.println("Detected a min distance change at Entity1... broadcasting to other nodes.\n");
+            for (int i = 1; i < numEntities; i++) {
+                Packet packet = new Packet(1, i, minDistances);
+                NetworkSimulator.toLayer2(packet);
+            }
+        }
+
+        // debug/info logging
+        System.out.println("Entity1 Updated at:" + NetworkSimulator.time + ". Distance Table is:\n");
+        printDT();
     }
     
     public void linkCostChangeHandler(int whichLink, int newCost)
     {
+        // reset the distance table because we cannot assume that we have accurate information in the DT
+        for (int i = 0 ; i < numEntities; i++) {
+            for (int j = 0; j < numEntities; j++) {
+                distanceTable[i][j] = 999;
+            }
+        }
+
+        // set the values in the distance table that we know from the diagram
+        // 1 is not connected to three so we initialize it to inf
+        // TODO if time: would be nice to make this a little more dynamic. Hard code for now.
+        // Could set neighbors nodes and edges up top and calculate this maybe.
+        distanceTable[0][0] = 1;
+        distanceTable[1][1] = 0;
+        distanceTable[2][2] = 1;
+        distanceTable[3][3] = 999;
+
+        // take in the new cost into the DT
+        distanceTable[whichLink][whichLink] = newCost;
+
+        // calc min distances as before
+        // calculate our minimum costs, as in the init method
+        // TODO would be nice to extract this and reuse...
+        int[] minDistances = new int[numEntities];
+        for (int i = 0; i < numEntities; i++) {
+            int minDist = 999;
+            for (int j = 0; j < neighbors.length; j++) {
+                if (distanceTable[i][neighbors[j]] < minDist) {
+                    minDist = distanceTable[i][neighbors[j]];
+                }
+            }
+            minDistances[i] = minDist;
+        }
+        
+        // broadcast the result to neighbors
+        for(int i = 1; i < numEntities; i++) {
+            Packet packet = new Packet(1, i, minDistances);
+            NetworkSimulator.toLayer2(packet);
+        }
+
+        // debug/info logging
+        System.out.println("Entity1 Link Change Cost Completed at:" + NetworkSimulator.time + ". Distance Table is:\n");
+        printDT();
     }
     
     public void printDT()
